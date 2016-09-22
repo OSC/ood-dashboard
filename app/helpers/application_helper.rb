b547580916c6535b9c5f2880671d18d33809fcc1
Eric Franz <efranz@osc.edu>
2016-09-22 14:34:09 -0400

    remove unused files and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,26 +6,4 @@ module ApplicationHelper
   def login_clusters
     OodAppkit::Clusters.new(clusters.select(&:login_server?))
   end
-
-
-  def support_url
-    ENV['OOD_DASHBOARD_SUPPORT_URL'] || "#"
-  end
-
-  def docs_url
-    ENV['OOD_DASHBOARD_DOCS_URL'] || "#"
-  end
-
-  def passwd_url
-    ENV['OOD_DASHBOARD_PASSWD_URL'] || "#"
-  end
-
-  # FIXME: temporary solution
-  def render_nav
-    if ENV['OOD_DASHBOARD_SHOW_ALL_APPS']
-      render partial: "layouts/nav/nav"
-    else
-      render partial: "layouts/nav/simple_nav"
-    end
-  end
 end
