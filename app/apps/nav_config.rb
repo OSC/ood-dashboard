8f98875c360b29eccf0180190ef0dfd213c0773c
Eric Franz <efranz@osc.edu>
2016-11-09 16:06:09 -0500

    move nav Help dd config to initializer
    This is an example of moving the navbar Help dropdown configuration
    to the initializer. Instead of specifying a fixed number of URLs that can be specified,
    let the installer specify any number of links.

--- app/apps/nav_config.rb
+++ app/apps/nav_config.rb
@@ -1,6 +1,10 @@
 class NavConfig
   class << self
-    attr_accessor :categories
+    attr_accessor :categories, :help_dropdown_links, :user_dropdown_links
   end
   self.categories = ["Files", "Jobs", "Clusters", "Desktops"]
+
+  # array of NavLink objects
+  self.help_dropdown_links = []
+  self.user_dropdown_links = []
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,15 +11,6 @@ module ApplicationHelper
     "/nginx/stop?redir=#{root_path}"
   end
 
-
-  def support_url
-    ENV['OOD_DASHBOARD_SUPPORT_URL'] || "#"
-  end
-
-  def docs_url
-    ENV['OOD_DASHBOARD_DOCS_URL'] || "#"
-  end
-
   def passwd_url
     ENV['OOD_DASHBOARD_PASSWD_URL'] || "#"
   end
