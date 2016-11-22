0242ecfc5d37f54d4e687e75e0a42628d1b9fca1
Eric Franz <efranz@osc.edu>
2016-11-22 13:54:51 -0500

    add show format html alongside json

--- app/controllers/version_lists_controller.rb
+++ app/controllers/version_lists_controller.rb
@@ -3,10 +3,15 @@ class VersionListsController < ApplicationController
   end
 
   def show
-    path = params[:path] || SysRouter.base_path
+    @path = params[:path] || SysRouter.base_path
+    @apps = PathRouter.apps(@path).sort_by(&:name)
 
-    render json: PathRouter.apps(path).map { |app|
-      {:name => app.name, :remote => app.git_remote_origin_url, :version => app.git_version, :sha => app.git_sha, :user => nil, :group => nil, :permissions => nil}.merge(app.stat)
+    respond_to do |format|
+      format.html  # show.index.erb
+      format.json { render json: @apps.map { |app|
+        {:name => app.name, :remote => app.git_remote_origin_url, :version => app.git_version, :sha => app.git_sha, :user => nil, :group => nil, :permissions => nil}.merge(app.stat)
+      }
     }
+    end
   end
 end
