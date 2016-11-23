a991caaebf28ee12cdf2885a95fdb4fcef02719d
Eric Franz <efranz@osc.edu>
2016-11-23 11:42:54 -0500

    add build path to env

--- app/controllers/version_lists_controller.rb
+++ app/controllers/version_lists_controller.rb
@@ -1,10 +1,24 @@
 class VersionListsController < ApplicationController
   def index
+    @build_path = ENV['OOD_BUILD_PATH'] || params[:build_path]
+    raise "set query param ?build_path= or ENV['OOD_BUILD_PATH'] to valid directory: #{@build_path}" unless @build_path
+
+    @apps = apps.concat(apps(@build_path))
+
+    @apps = @apps.sort do |a,b|
+      if a.name == b.name
+        a.path.to_s <=> b.path.to_s
+      else
+        a.name <=> b.name
+      end
+    end
+
+    # [abaqus [/users/...], abaqus[/var/www],
   end
 
   def show
-    @path = params[:path] || SysRouter.base_path
-    @apps = PathRouter.apps(@path, require_manifest: false).sort_by(&:name)
+    @path = params[:path]
+    @apps = apps(@path)
 
     respond_to do |format|
       format.html  # show.index.erb
@@ -13,5 +27,12 @@ class VersionListsController < ApplicationController
       }
     }
     end
+  end
+
+
+  private
+
+  def apps(path = SysRouter.base_path)
+    PathRouter.apps(path, require_manifest: false).sort_by(&:name)
   end
 end
--- app/helpers/version_lists_helper.rb
+++ app/helpers/version_lists_helper.rb
@@ -1,2 +1,15 @@
 module VersionListsHelper
+  def compare_versions_github_link(app1, app2)
+    return "" if app1.git_version == app2.git_version
+    return "Git remotes differ" if app1.git_remote_origin_url != app2.git_remote_origin_url
+
+    rx = /((git@github\.com:)|(https:\/\/github\.com\/))(.*)\.git/
+
+    if app1.git_remote_origin_url =~ rx
+      repo = $4
+      link_to "#{app1.git_version}...#{app2.git_version}", ("https://github.com/%s/compare/%s...%s" % [repo, app1.git_version, app2.git_version]), target: "_blank"
+    else
+      "not a git repo"
+    end
+  end
 end
