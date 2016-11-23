bb7b5a5dee54b58ccc2d2cbde153fdf4d1f69349
Eric Franz <efranz@osc.edu>
2016-11-23 11:43:32 -0500

    fix comparing app remote git repos

--- app/helpers/version_lists_helper.rb
+++ app/helpers/version_lists_helper.rb
@@ -5,11 +5,15 @@ module VersionListsHelper
 
     rx = /((git@github\.com:)|(https:\/\/github\.com\/))(.*)\.git/
 
-    if app1.git_remote_origin_url =~ rx
+    # FIXME: the check to see if repos differ should be done prior to this
+    repo1 = app1.git_remote_origin_url.match(rx).captures.last
+    repo2 = app2.git_remote_origin_url.match(rx).captures.last
+
+    if repo1 && repo2 && repo1 == repo2
       repo = $4
       link_to "#{app1.git_version}...#{app2.git_version}", ("https://github.com/%s/compare/%s...%s" % [repo, app1.git_version, app2.git_version]), target: "_blank"
     else
-      "not a git repo"
+      "not a git repo or repos differ"
     end
   end
 end
