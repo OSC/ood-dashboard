1f6a4755299579f5d5231d0a5a73d51c959517b2
Jeremy Nicklas <jnicklas@osc.edu>
2017-07-18 12:10:10 -0400

    add back in local env for testing
    The `dotenv` gem removes the loading of `.env.local` in Rails test
    environment since version 2.2.0.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 ENV['OOD_CLUSTERS'] ||= 'test/fixtures/config/clusters.d'
+Dotenv.load Rails.root.join(".env.local") # add back in local environment for testing
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
