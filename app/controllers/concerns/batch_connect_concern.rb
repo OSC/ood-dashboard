d2c5c214b71fced43ccd89d3836ff221f8b7df27
Jeremy Nicklas <jnicklas@osc.edu>
2018-02-01 20:31:57 -0500

    fix for invalid shared apps

--- app/controllers/concerns/batch_connect_concern.rb
+++ app/controllers/concerns/batch_connect_concern.rb
@@ -8,12 +8,12 @@ module BatchConnectConcern
   end
 
   def bc_usr_app_groups
-    [
-      OodAppGroup.new(
-        title: "Shared Apps",
-        apps: nav_usr_apps.select(&:batch_connect_app?)
-      )
-    ]
+    apps = nav_usr_apps.select(&:batch_connect_app?)
+    if apps
+      [ OodAppGroup.new(title: "Shared Apps", apps: apps) ]
+    else
+      []
+    end
   end
 
   def bc_dev_app_groups
